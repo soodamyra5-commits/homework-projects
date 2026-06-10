0225158fee731cd8779ae5a5910a51390ac48920
soodamyra5-commits <soodamyra5@gmail.com>
2026-06-10 22:12:23 +0530

    My_dailt_routine_improvised.py

--- homework.py
+++ homework.py
@@ -1,15 +1,22 @@
 print("Hi, I am Amyra and would love ot share my everyday routine for the holidays")
+#Step 1: OPEN EYE SHUTTERS FORCEFULLY!!! 
 print("Waking up early in the morning is great for a fresh mind \n .....don't worry i never wake up early")
+#Step 2: YUM YUM!!
 print("We must eat a nutritious breakfast to charge ourselves for the day!!")
+#Step 3: STUDY TIME!!
 print("Forcefully after a scrumptous meal i am sent to none other but my annoying study table!!")
+#Step 4: FOOD AGAIN!!
 print("After a long time of looking around here and there without a single second focus on books.... \n ")
 print("You guessed it...its time for another meal!!!")
 print("Nothing special for luch but just a SALAD!! \n WHAT AN AWFUL DAY!!")
+#Step 5: SHOCKINGLY BRAIN SPARKS!!!
 print("Then in my mind something sparks...."  "The sum of",8,"+",9,"is",8+9)
 print("OMG....I DID IT!!")
 print("After a joyceful celebration of learning maths...it was time for my last meal.")
+#Step 6: PIZZA PIZZA!!
 print("Fortinately God was in  a good mood and my awfully strict doctor parents ordered pizza for me!!!")
 print("As i devourved the entire thing in seconds...i was yawning by the end of the the bite....")
+#Step 7: DESIRABLE THOUGHTS WITH SWEET DREAMS!!
 print("As i lay down in my bed i thought.. \n who could have such a busy schedule as me??")
 print("With that i cosily slept like a panda in hibernation!!")
 print("NOTE:THIS IS A COMPLETELY RANDOM SCHEDULE SO EVEN IF U LOVE IT...PLZ DON'T FOLLOW IT!!")
