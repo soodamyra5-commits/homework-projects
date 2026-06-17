0a45093c71e9360c659190608cb8e091533fdf82
soodamyra5-commits <soodamyra5@gmail.com>
2026-06-17 15:19:05 +0530

    birthdaydates.py

---
+++ congratulation.py
@@ -0,0 +1,8 @@
+#Take the lowercase congratulations as input
+lowercase_message=input("Enter the congratulations message: ")
+
+#Convert the entire thing into uppercase using the upper( method)
+uppercase_message= lowercase_message.upper()
+#Display the converted uppercase message 
+print("\n Converted message")
+print(uppercase_message)
