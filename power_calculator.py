d7167d9bed1d2124dc3460a2248d90640765c711
soodamyra5-commits <soodamyra5@gmail.com>
2026-07-28 21:02:56 +0530

    loop.py

--- power_calculator.py
+++ power_calculator.py
@@ -1,7 +1,11 @@
 print("--------POWER CALCULATOR------")
 base= float(input("Enter any base number :"))
-exponent=float(input("Enter the power/exponention:"))
-result=base**exponent
+exponent=int(input("Enter the power/exponention (integer):"))
+result=1.0
+for _ in range(abs(exponent)):
+    result*=base
+if exponent < 0 :
+    result= 1.0 / result
 print(f"{base}raised to the power of {exponent} is:{result}")
 
 
